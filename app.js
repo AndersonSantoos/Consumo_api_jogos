c518ae06dba272901198d08a2ae4ad63e5605863
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-11-06 19:41:30 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 5000; 
+const port = 5000;
 
 app.use(express.static('public'));
 
 // Rota para a página de login
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/login.html'));
+    res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
 // Rota para a página principal
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Rota para a raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(port, () => {
